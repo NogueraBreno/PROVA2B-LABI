4bf084c8ccf7efb802032cfd23a7697f07fde673
Breno Nogueira <brenozxc@hotmail.com>
2017-12-04 19:48:43 -0300

    att

--- WebAppPB2 - Breno nogueira -/Models/Evento.cs
+++ WebAppPB2 - Breno nogueira -/Models/Evento.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,12 @@ namespace WebAppPB2___Breno_nogueira__.Models
 {
     public class Evento
     {
+        [Key]
+        [Column(Order = 1)]
+        string Id;
+        [Required(ErrorMessage = "Titulo Obrigatorio")]
+        string Titulo;
+        [Required(ErrorMessage = "Tipo Obrigatorio")]
+        Tipo Tipo;
     }
 }
---
+++ WebAppPB2 - Breno nogueira -/Models/Tipo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppPB2___Breno_nogueira__.Models
+{
+    public enum Tipo
+    {
+
+        Nacional = 1, Internacional = 2
+    
+    }
+}
